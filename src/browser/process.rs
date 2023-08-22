d2763be388237312c4495b23ebe34ccb52ee5d69
Jean-Matthieu BARBIER <jmbarbier@users.noreply.github.com>
2023-08-22 19:28:28 +0200

    Update process.rs to use headless=new

--- src/browser/process.rs
+++ src/browser/process.rs
@@ -346,7 +346,7 @@ impl Process {
         }
 
         if launch_options.headless {
-            args.extend(["--headless"]);
+            args.extend(["--headless=new"]);
         }
 
         if launch_options.ignore_certificate_errors {
